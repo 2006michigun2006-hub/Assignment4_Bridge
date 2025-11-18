8285790454cc72180aee20fb239e390cbff6631e
Sanzhar Baiserikov <2006michigun2006@gmail.com>
2025-11-18 07:57:55 +0500

    Update Main.java

--- Assignment4_Bridge/Main.java
+++ Assignment4_Bridge/Main.java
@@ -1,14 +1,41 @@
-public class Main {
-    public static void main(String[] args) {
-        OrganImplementation bioImpl = new BiologicalHeart();
-        OrganImplementation mechImpl = new MechanicalHeart();
+public class Bridge {
+    //Abstract product
+    public static abstract class Heart {
+        protected HeartAction action;
 
-        Heart bioHeart = new Heart("Red", "Dimash", 5, 1337228, bioImpl);
-        Heart mechHeart = new Heart("Grey", "RoboBeka", 10, 1488228, mechImpl);
-
-        bioHeart.votHeart();
-        bioHeart.operate();
-        mechHeart.votHeart();
-        mechHeart.operate();
+        public Heart(HeartAction action) {
+            this.action = action;
+        }
+        public abstract void work();
+    }
+    //Concrete product
+    public static class HumanHeart extends Heart {
+        public HumanHeart(HeartAction action) {
+            super(action);
+        }
+        @Override
+        public void work() {
+            action.beat();
+            action.pulse();
+        }
+    }
+    //Abstract Bridge
+    public static abstract interface HeartAction {
+        void beat();
+        void pulse();
+    }
+    //Concrete Bridge
+    public static class FastAction implements HeartAction {
+        @Override
+        public void beat() {
+        }
+        @Override
+        public void pulse() {
+        }
+    }
+    //Client
+    public static void main(String[] poop) {
+        Heart heart = new HumanHeart(new FastAction());
+        heart.work();
     }
 }
